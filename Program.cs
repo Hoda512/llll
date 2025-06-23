8a40facbe32fd40e51cd9f511b64f5b0fd348f00
Mahmoud Diab <144825616+Hoda512@users.noreply.github.com>
2025-06-23 16:54:35 +0300

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Ip ip = new Ip(1, 2, 3, 4);
+            ip[0] = 9;
+            Console.WriteLine(ip.dis);
+        }
+    }
+
+
+    public class Ip {
+
+        private int[] Segmants = new int[4];
+
+
+
+
+        public int this[int index] {
+
+            get { 
+            
+            return Segmants[index];
+            }
+            set {
+
+                Segmants[index] = value;
+            
+            }
+        
+        
+        }
+        public Ip(int segmant1, int segmant2, int segmant3, int segmant4) {
+
+            Segmants[0] = segmant1;
+            Segmants[1] = segmant2;
+            Segmants[2] = segmant3;
+            Segmants[3] = segmant4;
+
+        }
+        public string dis =>string.Join(".", Segmants);
+    
+    
+    
+    }
+}
